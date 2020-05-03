b34562cd2d430e754c48bb4e20a025707a9e65a3
Shwopno Tori <shwopnotori@gmail.com>
2020-05-03 17:31:29 -0400

    pom basic done

--- POM_CNN/src/main/java/pages/Homepage.java
+++ POM_CNN/src/main/java/pages/Homepage.java
@@ -1,8 +1,69 @@
 package pages;
 
 import base.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 public class Homepage extends CommonAPI
 {
+    @FindBy(how = How.LINK_TEXT, using = "videos")
+    public static WebElement videosMenuBar;
 
+    @FindBy(how = How.CSS, using = "button.sc-jDwBTQ:nth-child(2) > svg:nth-child(1)")
+    public static WebElement searchButton;
+
+    @FindBy(how = How.CSS, using = ".menu-icon")
+    public static WebElement menuBarWebElement;
+
+    @FindBy(how = How.CSS, using = "button.sc-jDwBTQ:nth-child(2) > svg")
+    public static WebElement searchWebElement;
+
+    @FindBy(how = How.CSS, using = "div.Flex-sc-1sqrs56-0:nth-child(6) > div:nth-child(1) > svg:nth-child(1)")
+    public static WebElement userIconWebElement;
+
+    @FindBy(how = How.CSS, using = ".indexes__LiveTVWrap-nujtvs-15 > a:nth-child(1) > span")
+    public static WebElement liveTVWebElement;
+
+
+//    public Homepage()
+//    {
+//        PageFactory.initElements(driver, Homepage.class);
+//    }
+
+    public void clickOnVideoMenu()
+    {
+        videosMenuBar.click();
+    }
+
+    public void clickOnSearchBox()
+    {
+        searchButton.click();
+    }
+
+    public void clickOnMenuBar()
+    {
+        menuBarWebElement.click();
+    }
+
+    public void clickOnSearchWeb()
+    {
+        searchWebElement.click();
+    }
+
+    public void clickOnUserIcon()
+    {
+        userIconWebElement.click();
+    }
+
+    public void clickOnLiveTV()
+    {
+        liveTVWebElement.click();
+    }
+
+    public void verifyHomePageTitle()
+    {
+        System.out.println(driver.getTitle());
+    }
 }
--- POM_CNN/src/test/java/testing/TestingHomepage.java
+++ POM_CNN/src/test/java/testing/TestingHomepage.java
@@ -1,13 +1,55 @@
 package testing;
 
 import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+import pages.Homepage;
 
 public class TestingHomepage extends CommonAPI
 {
-    @Test
-    public void news()
+
+    /****
+     @Test(priority=1)
+     public void verifyHomePageTitleTest(){
+     String homePageTitle = homePage.verifyHomePageTitle();
+     Assert.assertEquals(homePageTitle, "CRMPRO","Home page title not matched");
+     }
+
+     @Test(priority=2)
+     public void verifyUserNameTest(){
+     testUtil.switchToFrame();
+     Assert.assertTrue(homePage.verifyCorrectUserName());
+     }
+
+     @Test(priority=3)
+     public void verifyContactsLinkTest(){
+     testUtil.switchToFrame();
+     contactsPage = homePage.clickOnContactsLink();
+     }
+     ***/
+
+    Homepage homepage = null;
+
+
+
+    public void pageFactory()
     {
-        System.out.println(driver.getTitle());
+        homepage = PageFactory.initElements(driver, Homepage.class);
     }
+
+    @Test (priority = 1)
+    public void verifyHomePageTitleTest()
+    {
+        pageFactory();
+        homepage.verifyHomePageTitle();
+    }
+
+    @Test (priority = 2)
+    public void verifyLiveTVButton() throws InterruptedException
+    {
+        pageFactory();
+        homepage.clickOnLiveTV();
+        sleepFor(10);
+    }
+
 }
