0c9d7b100b096723705871ab22ccab506a1cdae5
Shwopno Tori <shwopnotori@gmail.com>
2020-05-03 17:41:31 -0400

    comments deleted

--- POM_CNN/src/test/java/testing/TestingHomepage.java
+++ POM_CNN/src/test/java/testing/TestingHomepage.java
@@ -7,30 +7,7 @@ import pages.Homepage;
 
 public class TestingHomepage extends CommonAPI
 {
-
-    /****
-     @Test(priority=1)
-     public void verifyHomePageTitleTest(){
-     String homePageTitle = homePage.verifyHomePageTitle();
-     Assert.assertEquals(homePageTitle, "CRMPRO","Home page title not matched");
-     }
-
-     @Test(priority=2)
-     public void verifyUserNameTest(){
-     testUtil.switchToFrame();
-     Assert.assertTrue(homePage.verifyCorrectUserName());
-     }
-
-     @Test(priority=3)
-     public void verifyContactsLinkTest(){
-     testUtil.switchToFrame();
-     contactsPage = homePage.clickOnContactsLink();
-     }
-     ***/
-
     Homepage homepage = null;
-
-
 
     public void pageFactory()
     {
