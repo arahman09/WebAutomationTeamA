145aaffb56822195126ec637d679047c5d7c3c6e
arahman09 <sajolusa84@gmail.com>
2020-05-09 23:00:42 -0400

    test

---
+++ Microsoft/src/main/java/pages/HomePage.java
@@ -0,0 +1,148 @@
+package pages;
+
+import base.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.testng.annotations.Test;
+import reporting.TestLogger;
+
+import java.io.IOException;
+
+public class HomePage extends CommonAPI {
+
+    @FindBy(how = How.CSS, using = "#shellmenu_0")
+    public static WebElement microsoft365WebElement;
+    @FindBy(how = How.CSS, using = "#shellmenu_1")
+    public static WebElement officeWebElement;
+    @FindBy(how = How.CSS, using = "#shellmenu_2")
+    public static WebElement windowsWebElement;
+    @FindBy(how = How.CSS, using = "#shellmenu_3")
+    public static WebElement surfaceWebElement;
+    @FindBy(how = How.CSS, using = "#shellmenu_4")
+    public static WebElement xboxWebElement;
+    @FindBy(how = How.CSS, using = "#shellmenu_5")
+    public static WebElement dealsWebElement;
+    @FindBy(how = How.XPATH, using = "//a[@id='l1_support']")
+    public static WebElement supportWebElement;
+    @FindBy(how = How.XPATH, using = "//*[@id=\"uhf-c-nav\"]/ul/li/div/button/span")
+    public WebElement allMicrosoftWebElement;
+    @FindBy (how = How.CSS, using = "#search")
+    public WebElement searchWebElement;
+    @FindBy (how = How.XPATH, using = "//*[@id='uhf-shopping-cart']")
+    public WebElement cartWebElement;
+    @FindBy (how = How.XPATH, using = "//*[@id='mectrl_main_trigger']/div/div[1]")
+    public WebElement signInWebElement;
+
+    public WebElement getMicrosoft365WebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        return microsoft365WebElement;
+    }
+    public WebElement getOfficeWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        return officeWebElement;
+    }
+    public WebElement getWindowsWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        return windowsWebElement;
+    }
+    public WebElement getSurfaceWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        return surfaceWebElement;
+    }
+    public WebElement getXboxWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        return xboxWebElement;
+    }
+    public WebElement getDealsWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        return dealsWebElement;
+    }
+    public WebElement getAllMicrosoftWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        return allMicrosoftWebElement;
+    }
+    public WebElement getSearchWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        return searchWebElement;
+    }
+    public WebElement getCartWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        return cartWebElement;
+    }
+    public WebElement getSignInWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        return signInWebElement;
+    }
+    public WebElement getSupportWebElement() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        return supportWebElement;
+    }
+    public void clickOnMicrosoft365() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        getMicrosoft365WebElement().click();
+    }
+    public void clickOnOffice() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        getOfficeWebElement().click();
+    }
+    public void clickOnWindows() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        getWindowsWebElement().click();
+    }
+    public void clickOnSurface() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        getSurfaceWebElement().click();
+    }
+    public void clickOnXbox() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        getXboxWebElement().click();
+    }
+    public void clickOnDeal() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        getDealsWebElement();
+    }
+    public void clickOnSupport() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        getSupportWebElement().click();
+    }
+    public void clickOnAllMicrosoft() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        getAllMicrosoftWebElement().click();
+    }
+    public void clickOnSearch() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        getSearchWebElement().click();
+    }
+    public void clickOnCart() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        getCartWebElement().click();
+    }
+    public void clickOnSignIn() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object() {
+        }.getClass().getEnclosingMethod().getName()));
+        getSignInWebElement().click();
+    }
+
+}
---
+++ Microsoft/src/test/java/HomePage/TestHomePage.java
@@ -0,0 +1,85 @@
+package HomePage;
+
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import pages.HomePage;
+import reporting.TestLogger;
+
+public class TestHomePage extends CommonAPI {
+
+    HomePage homePage= null;
+
+    @Test (priority = 1)
+    public void verifyTitle(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homePage = PageFactory.initElements(driver, HomePage.class);
+        System.out.println("Microsoft Web Page Title is: " + driver.getTitle());
+    }
+    @Test (priority = 2)
+    public void microsoft365Tab() {
+        homePage = PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnMicrosoft365();
+    }
+    @Test (priority = 3)
+    public void OfficeTab(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homePage = PageFactory.initElements(driver,HomePage.class);
+        homePage.clickOnOffice();
+    }
+    @Test (priority = 4)
+    public void windowsTab() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homePage = PageFactory.initElements(driver,HomePage.class);
+        homePage.clickOnWindows();
+    }
+    @Test(priority = 5)
+    public void surfaceTab(){
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homePage = PageFactory.initElements(driver,HomePage.class);
+        homePage.clickOnSurface();
+    }
+    @Test(priority = 6)
+    public void xboxTab() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homePage = PageFactory.initElements(driver,HomePage.class);
+        homePage.clickOnXbox();
+    }
+    @Test(priority = 7)
+    public void dealTab() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homePage = PageFactory.initElements(driver,HomePage.class);
+        homePage.clickOnDeal();
+    }
+    @Test (priority =8)
+    public void supportTab() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homePage = PageFactory.initElements(driver,HomePage.class);
+        homePage.clickOnSupport();
+    }
+    @Test (priority = 9)
+    public void allMicrosoftTab() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homePage = PageFactory.initElements(driver,HomePage.class);
+        homePage.clickOnAllMicrosoft();
+    }
+    @Test (priority = 10)
+    public void searchTab() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homePage = PageFactory.initElements(driver,HomePage.class);
+        homePage.clickOnSearch();
+    }
+    @Test (priority = 11)
+    public void cartTab()  {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homePage= PageFactory.initElements(driver, HomePage.class);
+        homePage.clickOnCart();
+
+    }
+    @Test (priority = 12)
+    public void signInTab() {
+        TestLogger.log(getClass().getSimpleName() + ": " + CommonAPI.convertToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        homePage = PageFactory.initElements(driver,HomePage.class);
+        homePage.clickOnSignIn();
+    }
+}
